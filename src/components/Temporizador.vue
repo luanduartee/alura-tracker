3da9b3bfad168a93c6a3b47f9e61673d220c4eb4
luanduartee <luanmateus.duarte@gmail.com>
2023-07-28 10:09:55 -0300

    add: criação do componente Botao

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,30 +1,22 @@
 <template>
-    <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <section class="is-flex is-align-items-center is-justify-content-space-between">
         <Cronometro :tempoEmSegundos="tempoEmSegundos" />
-        <button class="button" @click="iniciar" :disabled="cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-        </button>
-        <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-        </button>
-    </div>
+        <Botao @clicado="iniciar" :desabilitado="cronometroRodando" icone="fas fa-play" texto="play"/>
+        <Botao @clicado="finalizar" :desabilitado="!cronometroRodando" icone="fas fa-play" texto="stop"/>
+    </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Cronometro from './Cronometro.vue';
+import Botao from './Botao.vue';
 
 export default defineComponent({
     name: 'Temporizador-Name',
     components: {
-        Cronometro,
-    },
+    Cronometro,
+    Botao
+},
     emits: ['aoTemporizadorFinalizado'],
     data() {
         return {
