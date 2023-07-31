1582bb6dbff939747e650e56cfeb8c7d38c11b29
luanduartee <luanmateus.duarte@gmail.com>
2023-07-31 10:35:13 -0300

    add: criação de estrutura de rotas de tarefas

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,7 @@
       <BarraLateral @aoTemaAlterado="trocarTema" />
     </div>
     <div class="column is-three-quarter conteudo">
-      <Formulario @aoSalvarTarefa="salvarTarefa" />
-      <div class="lista">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
-      </div>
-      <Box v-if="listaEstaVazia">
-        Você não está muito produtivo hoje :(
-      </Box>
+      <router-view></router-view>
     </div>
   </main>
 </template>
@@ -18,34 +12,19 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
-import Formulario from './components/Formulario.vue';
-import Tarefa from './components/Tarefa.vue';
 import ITarefa from './interfaces/ITarefa';
-import Box from './components/Box.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     BarraLateral,
-    Formulario,
-    Tarefa,
-    Box
-  },
-  computed: {
-    listaEstaVazia(): boolean {
-      return this.tarefas.length === 0
-    }
   },
   data() {
     return {
-      tarefas: [] as ITarefa[],
       modoEscuroAtivo: false
     }
   },
   methods: {
-    salvarTarefa(tarefa: ITarefa) {
-      this.tarefas.push(tarefa)
-    },
     trocarTema(modoEscuroAtivo: boolean) {
       this.modoEscuroAtivo = modoEscuroAtivo
     }
@@ -70,4 +49,5 @@ main.modo-escuro {
 
 .conteudo {
   background-color: var(--bg-primario);
-}</style>
+}
+</style>
