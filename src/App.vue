51d27cc5d9ab2d07e583d132216e23188fc0f596
luanduartee <luanmateus.duarte@gmail.com>
2023-07-31 07:43:54 -0300

    update: tratando lista quando não se tem tarefas

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,11 @@
     <div class="column is-three-quarter">
       <Formulario @aoSalvarTarefa="salvarTarefa" />
       <div class="lista">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
+        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
       </div>
+      <Box v-if="listaEstaVazia">
+        Você não está muito produtivo hoje :(
+      </Box>
     </div>
   </main>
 </template>
@@ -18,21 +21,28 @@ import BarraLateral from './components/BarraLateral.vue';
 import Formulario from './components/Formulario.vue';
 import Tarefa from './components/Tarefa.vue';
 import ITarefa from './interfaces/ITarefa';
+import Box from './components/Box.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     BarraLateral,
     Formulario,
-    Tarefa
+    Tarefa,
+    Box
   },
-  data () {
+  computed: {
+    listaEstaVazia () : boolean {
+      return this.tarefas.length === 0
+    }
+  },
+  data() {
     return {
       tarefas: [] as ITarefa[]
     }
   },
   methods: {
-    salvarTarefa (tarefa: ITarefa) {
+    salvarTarefa(tarefa: ITarefa) {
       this.tarefas.push(tarefa)
     }
   }
